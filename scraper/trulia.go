4682ef9695db35d38dffafcd49d7ade95ebbee08
ZHCode <admin@zh-code.com>
2020-06-05 18:54:09 -0400

    It's working but they have certain limit

--- scraper/trulia.go
+++ scraper/trulia.go
@@ -20,8 +20,9 @@ import (
 
 //Trulia https://www.trulia.com/
 type Trulia struct {
-	elements   []sitemap.Element
-	gzSitemaps []string
+	elements        []sitemap.Element
+	gzSitemaps      []sitemap.Element
+	currentGzMapURL *sitemap.Element
 
 	q *queue.Queue
 }
@@ -34,56 +35,54 @@ var (
 func (t *Trulia) Start() {
 	count := 0
 	t.q, _ = queue.New(
-		2,
+		10,
 		&queue.InMemoryQueueStorage{MaxSize: 10000},
 	)
 
 	c := colly.NewCollector(
-		colly.UserAgent("Real Estate Agent"),
+		colly.UserAgent("Googlebot"),
 		colly.AllowURLRevisit(),
 	)
-	rp, err := proxy.RoundRobinProxySwitcher(os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"),
-		os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"), os.Getenv("TRULIA_PROXY"))
+	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 10})
+	rp, err := proxy.RoundRobinProxySwitcher(os.Getenv("TRULIA_PROXY"))
 	if err != nil {
 		utils.Logger.Fatal(err.Error())
 	}
 	c.SetProxyFunc(rp)
-	c.OnHTML("script", func(e *colly.HTMLElement) {
-		if e.Attr("id") == "__NEXT_DATA__" {
-			appData := &truliaRawObject{}
-			err := json.Unmarshal([]byte(e.Text), &appData)
-			if err != nil {
-				utils.Logger.Error(err.Error())
-				return
-			}
+	c.OnHTML("script[id=__NEXT_DATA__]", func(e *colly.HTMLElement) {
+		appData := &truliaRawObject{}
+		err := json.Unmarshal([]byte(e.Text), &appData)
+		if err != nil {
+			utils.Logger.Error(err.Error())
+			return
+		}
 
-			var b bytes.Buffer
-			gz := gzip.NewWriter(&b)
-			jsonBytes, err := json.Marshal(appData.Props)
-			if err != nil {
-				utils.Logger.Error(err.Error())
-				return
-			}
-			_, err = gz.Write(jsonBytes)
-			if err != nil {
-				utils.Logger.Error(err.Error())
-				return
-			}
-			gz.Close()
-			truliaEntity := &db.Trulia{
-				URL: appData.Props.AsPath,
-			}
-			if err = truliaEntity.Find(); err != nil {
-				truliaEntity.State = strings.ToUpper(appData.Props.HomeDetails.Location.StateCode)
-				truliaEntity.City = strings.ToUpper(appData.Props.HomeDetails.Location.City)
-				truliaEntity.Data = b.Bytes()
-			} else {
-				truliaEntity.Data = b.Bytes()
-			}
-			err = truliaEntity.Save()
-			if err != nil {
-				utils.Logger.Error(err.Error())
-			}
+		var b bytes.Buffer
+		gz := gzip.NewWriter(&b)
+		jsonBytes, err := json.Marshal(appData.Props)
+		if err != nil {
+			utils.Logger.Error(err.Error())
+			return
+		}
+		_, err = gz.Write(jsonBytes)
+		if err != nil {
+			utils.Logger.Error(err.Error())
+			return
+		}
+		gz.Close()
+		truliaEntity := &db.Trulia{
+			URL: appData.Props.AsPath,
+		}
+		if err = truliaEntity.Find(); err != nil {
+			truliaEntity.State = strings.ToUpper(appData.Props.HomeDetails.Location.StateCode)
+			truliaEntity.City = strings.ToUpper(appData.Props.HomeDetails.Location.City)
+			truliaEntity.Data = b.Bytes()
+		} else {
+			truliaEntity.Data = b.Bytes()
+		}
+		err = truliaEntity.Save()
+		if err != nil {
+			utils.Logger.Error(err.Error())
 		}
 	})
 	c.OnRequest(func(r *colly.Request) {
@@ -119,27 +118,35 @@ func (Trulia) getRedisTimeUpdatedKey(url string) string {
 func (t *Trulia) loadQueue() {
 	for {
 		size, err := t.q.Size()
-		if err != nil || size > 200 {
+		if err != nil || size > 20 {
 			return
 		}
 		var e sitemap.Element
 		for {
 			if len(t.elements) > 0 {
 				e = t.elements[0]
+				t.currentGzMapURL = &e
 				t.elements = t.elements[1:]
 				break
 			}
-
+			if t.currentGzMapURL != nil {
+				gzMapKey := t.getRedisTimeUpdatedKey(t.currentGzMapURL.Loc)
+				utils.RedisSet(gzMapKey, t.currentGzMapURL.LastMod, 24*time.Hour)
+				t.currentGzMapURL = nil
+			}
 			if len(t.gzSitemaps) > 0 {
 				gzSM := t.gzSitemaps[0]
+				t.gzSitemaps = t.gzSitemaps[1:]
+				if t.redisUpdated(gzSM.Loc, gzSM.LastMod) {
+					continue
+				}
 				utils.Logger.Sugar().Info("Start loading urls from sitemap ", gzSM)
-				sm, err := sitemap.GetSitemapGZ(gzSM)
+				sm, err := sitemap.GetSitemapGZ(gzSM.Loc)
 				if err != nil {
 					utils.Logger.Error(err.Error())
 					return
 				}
 				utils.Logger.Sugar().Infof("%d urls loaded from sitemap", len(sm.Elements))
-				t.gzSitemaps = t.gzSitemaps[1:]
 				for _, r := range sm.Elements {
 					t.elements = append(t.elements, r)
 				}
@@ -151,19 +158,8 @@ func (t *Trulia) loadQueue() {
 		}
 		relativePath := strings.Replace(e.Loc, "https://www.trulia.com", "", 1)
 		key := t.getRedisTimeUpdatedKey(relativePath)
-		if utils.RedisExist(key) {
-			timeStr, err := utils.RedisGetString(key)
-			if err != nil {
-				utils.Logger.Error(err.Error())
-			} else {
-				redisTime, _ := time.Parse(time.RFC3339, timeStr)
-				mapTime, _ := time.Parse(time.RFC3339, e.LastMod)
-				if mapTime.After(redisTime) {
-					utils.RedisSet(key, e.LastMod, 24*time.Hour)
-				} else {
-					continue
-				}
-			}
+		if t.redisUpdated(relativePath, e.LastMod) {
+			continue
 		} else {
 			utils.RedisSet(key, e.LastMod, 24*time.Hour)
 		}
@@ -172,6 +168,23 @@ func (t *Trulia) loadQueue() {
 			utils.Logger.Error(err.Error())
 		}
 	}
+}
+
+func (t *Trulia) redisUpdated(url, updatedTime string) bool {
+	key := t.getRedisTimeUpdatedKey(url)
+	if utils.RedisExist(key) {
+		timeStr, err := utils.RedisGetString(key)
+		if err != nil {
+			utils.Logger.Error(err.Error())
+		} else {
+			redisTime, _ := time.Parse(time.RFC3339, timeStr)
+			mapTime, _ := time.Parse(time.RFC3339, updatedTime)
+			if mapTime.Before(redisTime) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func (t *Trulia) getIndexes() error {
@@ -183,7 +196,7 @@ func (t *Trulia) getIndexes() error {
 		return err
 	}
 	for _, r := range prIndex.Elements {
-		t.gzSitemaps = append(t.gzSitemaps, r.Loc)
+		t.gzSitemaps = append(t.gzSitemaps, r)
 	}
 
 	//recent updates
@@ -194,7 +207,7 @@ func (t *Trulia) getIndexes() error {
 	}
 	utils.Logger.Info("Loading sitemaps...")
 	for _, r := range ruIndex.Elements {
-		t.gzSitemaps = append(t.gzSitemaps, r.Loc)
+		t.gzSitemaps = append(t.gzSitemaps, r)
 	}
 	return nil
 }
